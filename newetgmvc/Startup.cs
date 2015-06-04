7fa4ef957eb2e48ff529f76e964440b7ec5d1eaf
Hanane Moshe <hananemo@gmail.com>
2015-06-04 11:32:28 -0400

    initial commit

---
+++ newetgmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(newetgmvc.Startup))]
+namespace newetgmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
